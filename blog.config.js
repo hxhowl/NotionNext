c3ff9c241812906fbcafebebc55c5c1f0cf50716
hxhowl <zcanfly@126.com>
2024-01-29 12:57:19 +0800

    基本配置调整:配置google、baidu的seo参数

--- blog.config.js
+++ blog.config.js
@@ -349,10 +349,10 @@ const BLOG = {
   ANALYTICS_ACKEE_DOMAIN_ID: process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_DOMAIN_ID || '', // e.g '82e51db6-dec2-423a-b7c9-b4ff7ebb3302'
 
   SEO_GOOGLE_SITE_VERIFICATION:
-        process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
+        process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || 'codeva-ISUlK8uJf3', // Remove the value or replace it with your own google site verification code
 
   SEO_BAIDU_SITE_VERIFICATION:
-        process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
+        process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || 'google568dd6e9a3b0d878.html', // Remove the value or replace it with your own google site verification code
 
   // <---- 站点统计
 
